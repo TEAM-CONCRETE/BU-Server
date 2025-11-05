3a78ffc051f15adaca263d4b71e9e412811462db
hyuunminn <132858983+moonhyeonmin@users.noreply.github.com>
2025-11-05 14:02:16 +0900

    feat: S3 설정 클래스 구현
    - S3Client Bean 설정: S3와의 기본 통신을 위한 클라이언트
    - S3Presigner Bean 설정: Presigned URL 발급을 위한 클라이언트
    - AWS 자격증명 및 리전 설정을 application.yml에서 주입
    - StaticCredentialsProvider를 통한 자격증명 관리

---
+++ src/main/java/com/concrete/buildup/global/config/S3Config.java
@@ -0,0 +1,56 @@
+package com.concrete.buildup.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+/**
+ * AWS S3 설정 클래스
+ * S3 클라이언트 및 Presigned URL 생성기를 Bean으로 등록합니다.
+ */
+@Configuration
+public class S3Config {
+
+    @Value("${spring.cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${spring.cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.cloud.aws.region.static}")
+    private String region;
+
+    /**
+     * S3Client Bean
+     * S3와의 기본적인 통신을 위한 클라이언트
+     */
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
+                .build();
+    }
+
+    /**
+     * S3Presigner Bean
+     * Presigned URL 생성을 위한 클라이언트
+     * 클라이언트가 직접 S3에 업로드할 수 있는 임시 URL을 발급합니다.
+     */
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
+                .build();
+    }
+}
