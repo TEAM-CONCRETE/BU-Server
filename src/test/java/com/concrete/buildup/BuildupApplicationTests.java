33bd84dc80639d3baae969f92c16fd342ce13224
Sewon Kim <sewonkim1008@gmail.com>
2025-11-05 23:12:45 +0900

    test: 통합 테스트 환경 설정 개선
    - @ActiveProfiles 및 @TestPropertySource 추가로 H2 데이터베이스 사용 명시
    - 암호화 키를 정확히 32바이트로 수정

--- src/test/java/com/concrete/buildup/BuildupApplicationTests.java
+++ src/test/java/com/concrete/buildup/BuildupApplicationTests.java
@@ -2,8 +2,15 @@ package com.concrete.buildup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+	"spring.datasource.url=jdbc:h2:mem:testdb;MODE=MySQL;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+	"spring.datasource.driver-class-name=org.h2.Driver"
+})
 class BuildupApplicationTests {
 
 	@Test
