30e0c3513141074bb7421a71da98efaff2f30070
hyuunminn <132858983+moonhyeonmin@users.noreply.github.com>
2025-11-05 19:11:15 +0900

    test: S3 설정 관련 테스트 오류 해결

--- src/test/java/com/concrete/buildup/BuildupApplicationTests.java
+++ src/test/java/com/concrete/buildup/BuildupApplicationTests.java
@@ -1,10 +1,15 @@
 package com.concrete.buildup;
 
+import com.concrete.buildup.domain.upload.service.S3Service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class BuildupApplicationTests {
+
+	@MockBean
+	private S3Service s3Service;
 
 	@Test
 	void contextLoads() {
